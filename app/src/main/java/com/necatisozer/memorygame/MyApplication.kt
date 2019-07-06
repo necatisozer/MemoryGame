1d0441ab2a79214b147193f447a20df2b5dadd77
necati <necatisozer@gmail.com>
2019-07-06 16:22:04 +0300

    Init libraries

--- app/src/main/java/com/necatisozer/memorygame/MyApplication.kt
+++ app/src/main/java/com/necatisozer/memorygame/MyApplication.kt
@@ -1,13 +1,28 @@
 package com.necatisozer.memorygame
 
 import android.app.Application
+import com.facebook.stetho.Stetho
+import com.jakewharton.threetenabp.AndroidThreeTen
 import com.necatisozer.memorygame.di.ApplicationComponent
 import com.necatisozer.memorygame.di.DaggerApplicationComponent
 import com.necatisozer.memorygame.di.DaggerComponentProvider
+import com.necatisozer.memorygame.extension.debug
 import com.necatisozer.memorygame.extension.unsyncLazy
+import timber.log.Timber
 
 class MyApplication : Application(), DaggerComponentProvider {
     override val component: ApplicationComponent by unsyncLazy {
         DaggerApplicationComponent.factory().create(applicationContext)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        debug {
+            Stetho.initializeWithDefaults(this)
+            Timber.plant(Timber.DebugTree())
+        }
+
+        AndroidThreeTen.init(this)
     }
 }
